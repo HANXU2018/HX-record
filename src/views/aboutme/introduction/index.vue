313e72dc9ec0605be0abb141840d4027caf4328b
HANXU2018 <1076998404@qq.com>
2020-06-28 14:08:28 +0800

    Update index.vue

--- src/views/aboutme/introduction/index.vue
+++ src/views/aboutme/introduction/index.vue
@@ -1,12 +1,13 @@
 <template>
+
   <el-card class="box-card">
-    <div slot="header" class="clearfix">
-      <span>卡片名称</span>
-      <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
-    </div>
-    <div v-for="o in 4" :key="o" class="text item">
-      {{'列表内容 ' + o }}
-    </div>
+    <el-alert
+      title="前方正在施工"
+      type="error"
+      description="更多功能正在开发中敬请期待"
+      :closable="false"
+      show-icon>
+    </el-alert>
   </el-card>
 </template>
 
